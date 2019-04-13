f788a63710fba06051ffbf3046b1c6a58a9d23cf
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-13 14:40:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,3 +22,13 @@ def assign_rooms(array)
   end
   return newarray
 end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
